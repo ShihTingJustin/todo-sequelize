1be20bde35d6a681599ade74e1c68fdedac549fc
Justin Huang <justinhuang777@gmail.com>
2020-06-23 09:24:41 +0800

    feat: add login, register page and user modules

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const router = express.Router()
 
 const home = require('./modules/home')
+const users = require('./modules/users')
 
+router.use('/u', users)
 router.use('/', home)
 
 module.exports = router
---
+++ routes/modules/users.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/login', (req, res) => {
+  res.render('login')
+})
+
+router.post('/login', (req, res) => {
+
+})
+
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
+router.post('/register', (req, res) => {
+
+})
+
+router.get('/logout', (req, res) => {
+  
+})
+
+module.exports = router
