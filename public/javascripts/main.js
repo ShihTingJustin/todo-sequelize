cdcf8f7f07bc6a4a99463eb8ef798dcd6aaf0547
Justin Huang <justinhuang777@gmail.com>
2020-06-27 15:01:24 +0800

    feat: modify home router

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,3 +3,10 @@ window.setTimeout(function () {
   $(".alert").alert('close')
 }, 3000);
 
+
+const todoCard = document.querySelector('.todo-card')
+todoCard.addEventListener('click', e => {
+  if (e.target.classList.contains('todos')) {
+    console.log(123)
+  }
+})
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -6,20 +6,43 @@ const Todo = db.Todo
 const User = db.User
 
 router.get('/', (req, res) => {
-  const { id } = req.user
+  const UserId = req.user.id
 
-  User.findByPk(id)
+  User.findByPk(UserId)
     .then(user => {
       if (!user) throw new Error('user not found')
 
-      return Todo.findAll({
-        raw: true,
-        nest: true,
-        where: { UserId: id }
+      const findAllTodos = new Promise((resolve, reject) => {
+        Todo.findAll({
+          raw: true,
+          nest: true,
+          where: { UserId, isDone: false }
+        })
+          .then(todos => resolve(todos))
+          .catch(err => console.log(err))
       })
-        .then(todos => res.render('index', { todos }))
-        .catch(error => res.status(422).json(error))
+
+      const findAllDone = new Promise((resolve, reject) => {
+        Todo.findAll({
+          raw: true,
+          nest: true,
+          where: { UserId, isDone: true }
+        })
+          .then(dones => resolve(dones))
+          .catch(err => console.log(err))
+      })
+
+      async function setHomePage() {
+        const todos = await findAllTodos
+        const dones = await findAllDone
+
+        res.render('index', { todos, dones })
+      }
+
+      setHomePage()
+
     })
+    .catch(error => res.status(422).json(error))
 })
 
 module.exports = router
